1200da5f815e3af13796e2def6054c6afd9ab0b8
Eunchong Yu <kroisse@gmail.com>
2013-02-22 17:19:22 +0900

    Make contents_script to use GitHub API token
    ...that acquired in the options page.

--- githubstar/contents_script.js
+++ githubstar/contents_script.js
@@ -7,6 +7,25 @@ function get_html(data) {
 	return data.full_name + "<br />watchers [ " + data.watchers_count + " ] forks [ " + data.forks_count+" ]"
 }
 
+chrome.storage.local.get('github_auth', function (storage) {
+	if (!!storage.github_auth) {
+		$.ajaxSetup({
+			headers: {
+				'Authorization': 'token ' + storage.github_auth.token
+			}
+		});
+	}
+});
+
+$(document).ajaxComplete(function (e, xhr, options) {
+	var remaining = xhr.getResponseHeader('X-RateLimit-Remaining');
+	if (remaining !== null) {
+		var limit = xhr.getResponseHeader('X-RateLimit-Limit');
+		log('API calls remained: ' + remaining + '/' + limit);
+	}
+});
+
+
 var r = /(github\.com\/)([\w_\-\.]+)\/([\w_\-\.]+)/gmi;
 var loader = $('<div id="github-star-loader">Github Star</div>').css( {
 	display : "none",
